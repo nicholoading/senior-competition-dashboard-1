2e084bb22e1a087fe5be8f7333d696e4ff05bf3f
nicholoading <nicholas.ting.coding@gmail.com>
2025-04-02 18:38:32 +0800

    advertisement 1

--- components/ad.tsx
+++ components/ad.tsx
@@ -6,10 +6,10 @@ import { useState, useEffect } from "react";
 export function AdSlideshow() {
   // Array of images
   const images = [
-    "https://picsum.photos/1200/300?random=1",
-    "https://picsum.photos/1200/300?random=2",
-    "https://picsum.photos/1200/300?random=3",
-    "https://picsum.photos/1200/300?random=4",
+    "https://www.gstatic.com/youtube/img/promos/growth/YTP_logo_social_1200x630.png?days_since_epoch=20180",
+    "https://miro.medium.com/v2/resize:fit:1200/1*rEQGD7eciTUkoaePqyqY2A.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png",
+    "https://miro.medium.com/v2/resize:fit:1125/1*dDNpLKu_oTLzStsDTnkJ-g.png",
   ];
 
   // Array of corresponding links (random example links)
